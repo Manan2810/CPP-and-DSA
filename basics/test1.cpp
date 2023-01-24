5ef90158dd17e01f4d12b39aef7b220eff595068
Manan Mehra <mananmehra330@gmail.com>
2023-01-24 14:44:01 +0530

    commit6

--- basics/test1.cpp
+++ basics/test1.cpp
@@ -2,6 +2,39 @@
 using namespace std;
 int main()
 {
-    cout<<"my name is manan";
-    return 0;
+    int count;
+    int arr[100];
+    int n;
+    cin>>n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    // for(int i=0;i<n;i++)
+    // {
+    //     for(int j=i+1;j<n;j++)
+    //     {
+    //         if(arr[i]==arr[j])
+    //         {
+    //             count++;
+    //         }
+    //     }
+    // }
+    for (int i=0;i<n;i++)
+    {   
+        for(int j=i+1;j<n;j++)
+        {
+            if (arr[i]==arr[j])
+            {
+                arr[j]=arr[j+1];  
+                n--;
+                j++;
+           
+             }
+        }    
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i];
+    }
 }
