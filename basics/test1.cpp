69ded9c44b4af749a361c6443e340b222c61cfb2
Manan Mehra <mananmehra330@gmail.com>
2023-01-27 03:28:34 +0530

    commit6

--- basics/test1.cpp
+++ basics/test1.cpp
@@ -22,12 +22,21 @@ int main()
     // }
     for (int i=0;i<n;i++)
     {   
-        for(int j=i+1;j<n;j++)
+        for(int j=i;j<n;j++)
         {
             if (arr[i]==arr[j])
             {
-                arr[j]=arr[j+1];  
-                n--;
+                if ((j+1) == n)
+                {   n--;
+                    break;
+                }
+                else
+                {
+                    arr[j]=arr[j+1];  
+                    n--;
+
+                }
+                
            
              }
         }    
